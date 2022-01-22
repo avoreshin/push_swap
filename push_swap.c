b255de832cfa920e84be9ab8a7eefff0ea30769b
avoreshin <avoreshin@gmail.com>
2022-01-22 20:46:50 +0300

    fix: function

--- push_swap.c
+++ push_swap.c
@@ -6,34 +6,11 @@
 /*   By: jlamonic <jlamonic@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 20:56:42 by jlamonic          #+#    #+#             */
-/*   Updated: 2022/01/20 21:13:54 by jlamonic         ###   ########.fr       */
+/*   Updated: 2022/01/22 18:11:44 by jlamonic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/push_swap.h"
-
-void check(char **str)
-{
-	while (*str)
-	{
-		int i = atoi(*str);
-		str++;
-	}
-}
-
-int ft_split_argv(int argc, char **argv)
-{
-	char	**mas_str;
-	int		i;
-
-	i = 1;
-	while (i < argc)
-	{
-		mas_str = ft_split(argv[i], ' ');
-		check(mas_str);
-		i++;
-	}
-}
 
 int main (int argc, char **argv)
 {
@@ -47,7 +24,10 @@ int main (int argc, char **argv)
 	new->A = ft_split_argv(argc, argv);
 	if (ft_check_duplicate(new->A))
 		ft_error_mes();
+    if (ft_check_sorted(new->A))
+        exit (EXIT_FAILURE);
 	tmp = new->A;
 	count = ft_new_size(tmp);
 	ft_sort_stack(count, new);
+    ft_free_arr(&(new->A);
 }
